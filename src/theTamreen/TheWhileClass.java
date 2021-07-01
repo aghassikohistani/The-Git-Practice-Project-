59045879ff86fe2d41b6c1c9a11509c99415bda7
aghassi kohistani <aghassikohistani@gmail.com>
2021-07-01 16:55:51 -0700

    commiting these 2 new files

---
+++ src/theTamreen/TheWhileClass.java
@@ -0,0 +1,16 @@
+package theTamreen;
+
+public class TheWhileClass {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		while (a < b) {
+			b--;
+			System.out.println(a);
+		}
+
+	}
+
+}
