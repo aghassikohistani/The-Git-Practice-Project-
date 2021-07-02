1a9eac7affd4de67fa30a5fc8ef381cd4e84aafa
aghassi kohistani <aghassikohistani@gmail.com>
2021-07-01 18:32:41 -0700

    added the cart page

---
+++ src/theTamreen/Back.java
@@ -0,0 +1,13 @@
+package theTamreen;
+
+public class Back {
+
+	public static void main(String[] args) {
+
+
+		
+		System.out.println("this is the cart branch, and I am trying yo add some features here");
+
+	}
+
+}
---
+++ src/theTamreen/CartIphone.java
@@ -0,0 +1,15 @@
+package theTamreen;
+
+public class CartIphone {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+  public void featureIsReady() {
+	  System.out.println("this is the new feature");
+  }
+
+
+}
